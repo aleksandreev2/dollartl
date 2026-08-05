dfad83fc04d18ba4932ab78840ba542d29d47ab2
aleksandreev2 <sashahumortele3@gmail.com>
2026-08-05 04:10:56 +0400

    assemble v0.10 people operations router

---
+++ src/dollartl/admin/people_router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from dollartl.admin.people_batch import router as batch_router
+from dollartl.admin.people_dossier import router as dossier_router
+from dollartl.admin.people_moderation import router as moderation_router
+from dollartl.admin.people_selection import router as selection_router
+from dollartl.admin.people_users import router as users_router
+
+router = APIRouter(prefix="/admin/api", tags=["admin-people"])
+router.include_router(users_router)
+router.include_router(dossier_router)
+router.include_router(selection_router)
+router.include_router(batch_router)
+router.include_router(moderation_router)
